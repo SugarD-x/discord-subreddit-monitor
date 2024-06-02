95ac7e8f9897e12523c4e6fffc2d2c14448ce183
SugarD-x <sugajack2004@gmail.com>
2024-06-02 14:55:53 -0700

    Restoration from original project
    Restoration of the files from the original project.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Discord Subreddit Monitor
+This script monitors a subreddit for new posts and sends them as embeds to a Discord webhook
+## Getting Started
+
+1. Clone the repository
+2. Choose a subreddit and a prefix for the embed footer, and set them in config.json
+3. Create a webhook and add it to config.json
+4. ```npm install```
+5. ```npm start```
+
+### Prerequisites
+
+This bot was created using node.js, if you don't know node: [Install node.js!](https://nodejs.org/en/, "Install Node.js")
+
+## Contributing
+
+Contributing is more than welcome! 
+1. Fork the repository
+2. Create a new branch 
+3. Implement/fix whatever you want
+4. Make sure to use Eslint!
+5. Open a pull request:)
+
+## Planned additions:
+None, yet
+## Authors
+
+* **Jonas Scholz** - [Code42Cate](https://github.com/Code42Cate)
+* **Simon J.** - [shiftinv](https://github.com/shiftinv)
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used:D
---
+++ index.js
@@ -0,0 +1,45 @@
+const rp = require('request-promise-native');
+const fs = require('fs-extra');
+
+const config = fs.readJSONSync('config.json');
+
+let lastPost = '';
+
+const sendWebhook = (title, permalink, time) => {
+  console.log(time);
+  const payload = {
+    headers: { 'Content-Type': 'application/json' },
+    url: config.webhook,
+    method: 'POST',
+    json: {
+      embeds: [{
+        url: permalink,
+        title,
+        color: 0x16BE49,
+        footer: {
+          text: `${config.footerPrefix} @ ${time}`,
+        },
+      }],
+    },
+  };
+  rp(payload).catch(err => console.log(err));
+};
+
+const scrape = async () => {
+  rp(config.subredditURL).then((response) => {
+    const json = JSON.parse(response);
+    const latestPost = json.data.children[0].data.url;
+    if (lastPost !== latestPost) {
+      lastPost = latestPost;
+      sendWebhook(json.data.children[0].data.title, lastPost, new Date(json.data.children[0].data.created_utc * 1000).toUTCString());
+    }
+  }).catch(err => console.log(err));
+};
+
+const run = async () => {
+  setInterval(async () => {
+    await scrape();
+  }, 10000);
+};
+
+run();
